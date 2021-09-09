93786dc4b6832f660b30c87fbbd0b6194961263c
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-09-09 12:22:18 +0530

    Update Traversals.cpp

--- BinaryTree/Traversals.cpp
+++ BinaryTree/Traversals.cpp
@@ -1,1 +1,26 @@
+//Inorder Traversal
+ vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL){
+           inorderTraversal(root->left);
+           cout<<(root->val)<<" ";
+           inorderTraversal(root->right);
+        }
+ }
 
+//Preorder Traversal
+ vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL){
+           cout<<(root->val)<<" ";
+           inorderTraversal(root->left);
+           inorderTraversal(root->right);
+        }
+ }
+
+ //Postorder Traversal
+ vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL){
+           inorderTraversal(root->left);
+           inorderTraversal(root->right);
+           cout<<(root->val)<<" ";
+        }
+ }
