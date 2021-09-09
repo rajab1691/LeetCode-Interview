33a49bb86d9bb894fa08f867a20bb0ecac7f0f02
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-09-09 12:59:05 +0530

    Update Traversals.cpp

--- BinaryTree/Traversals.cpp
+++ BinaryTree/Traversals.cpp
@@ -24,3 +24,35 @@
            cout<<(root->val)<<" ";
         }
  }
+
+//LevelOrder Traversal
+vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        vector<vector<int>>res;
+        if(root==NULL)return res;
+        
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        while(!q.empty()){
+            
+            vector<int>ans;
+            int size=q.size();
+            
+            while(size--){
+            TreeNode* node=q.front();
+            q.pop();
+            ans.push_back(node->val);
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){    
+                q.push(node->right);
+            }
+        }
+            res.push_back(ans);
+        }
+        return res;
+    }
+
+
