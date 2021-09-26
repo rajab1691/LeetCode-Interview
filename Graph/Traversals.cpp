62eb7d5a3cdda2e27ab09675b12523172d7f6665
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-09-27 05:25:39 +0530

    Update Traversals.cpp

--- Graph/Traversals.cpp
+++ Graph/Traversals.cpp
@@ -1,1 +1,44 @@
+/*
+    BFS traversal (using queue)
+    Time complexity : O(V+E) 
+    adj=graph,v=vertex,src=starting point 
+*/
 
+void bfs(int src){
+
+    vector<bool>visit(v+1,false);
+    queue<int>q;
+    q.push(src);
+    visit[src]=true;
+
+    while(!q.empty()){
+
+        int node=q.front();
+        q.pop();
+        cout<<node<<" ";
+        for(auto x:adj[node]){
+            if(visit[x]==false){
+                q.push(x);
+                visit[x]=true;
+            }
+        }
+    }
+}
+
+/*
+   DFS Traversal(Using Recursion)
+   Time complexity: O(V+E)
+   adj=graph,v=vertex,src=starting point 
+   vector visit =false initially 
+*/
+void dfs(int src, vector<bool>visit){
+
+    visit[src]=true;
+    cout<<src<<" ";
+  
+    for(auto x:adj[src]){
+        if(visit[x]==false){
+            dfs(x,visit);
+        }
+    }
+}
