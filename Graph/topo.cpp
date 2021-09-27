c8b2dbe927908f4e5da5804b38ebd438cd335927
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-09-27 08:37:25 +0530

    Update topo.cpp

--- Graph/topo.cpp
+++ Graph/topo.cpp
@@ -1,1 +1,63 @@
+/*
+    DFS
+    ->just do dfs store elements and reverse it at the end
+*/
+void dfs(int src,vector<int>adj[],vector<bool>&visit,list<int>&order){
+
+    visit[src]=true;
+    for(auto x:adj[src]){
+        if(visit[x]==false){
+            dfs(x,adj,visit,order);
+        }
+    }   
+    order.push_front(src);
+}
+void topo(vector<int>adj[]){
+
+    vector<bool>visit(v,false);
+    list<int>order;
+
+    for(int i=0;i<v;i++){
+        if(visit[i]==false){
+            dfs(i,adj,visit,order);
+
+        }
+    }
+    for(auto x:order){
+        cout<<x<<" ";
+    }
+}
+
+/*
+    BFS
+    ->use Kahn's algo 
+*/
+void topo(vector<int>adj[]){
+
+    vector<int>indegree(v,0);
+
+    for(int i=0;i<v;i++){
+        for(auto x:adj[i]){
+            indegree[x]++;
+        }
+    }
+    queue<int>q;
+    for(int i=0;i<v;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    while(!q.empty()){
+
+        int node=q.front();
+        q.pop();
+        cout<<node<<" ";
+        for(auto x:adj[node]){
+            indegree[x]--;
+            if(indegree[x]==0){
+                q.push(x);
+            }
+        }
+    }
+}
 
