bbc639dfb350cf6be608a09779c237c09fdd3476
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-09-27 05:49:19 +0530

    Update countCon.cpp

--- Graph/countCon.cpp
+++ Graph/countCon.cpp
@@ -1,1 +1,14 @@
+/*
+    Use DFS to count the connected component
+    Time complexity =O(V+E)
+    
+*/
 
+ void dfs(int src, vector<bool>&visit) {
+        visit[src] = true;
+        for (auto x : adj[src]) {
+            if (visit[x] == false) {
+                dfs(x, visit);
+            }
+        }
+}
