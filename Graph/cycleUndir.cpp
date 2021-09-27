08c031a5c3bdb383b8cc846b0079ba2d0d3d1959
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-09-27 06:36:19 +0530

    Update cycleUndir.cpp

--- Graph/cycleUndir.cpp
+++ Graph/cycleUndir.cpp
@@ -1,6 +1,8 @@
 /*
+      DFS
       Time Complexity: O(V+E)
       Space Complexity: O(V)
+      v=nodes
 */
 bool dfs(int src,vector<int>adj[],vector<bool>&visit,int parent){
 
@@ -9,7 +11,7 @@ bool dfs(int src,vector<int>adj[],vector<bool>&visit,int parent){
         for(auto x:adj[src]){
             if(!visit[x]){
                 dfs(x,adj,visit,src);
-            }else if(x!=parent){
+            }else if(x!=parent){ //cycle detected
                 return true;
             }
         }
@@ -22,4 +24,32 @@ bool isCycle(vector<int>adj[]){
     vector<bool>visit(v+1,false);
 
     return dfs(src,adj,visit,parent);
+}
+
+/*
+      Time Complexity: O(V+E)
+      BFS
+*/
+bool isCycle(int src,bool*visit,vector<int>adj[]){
+
+    vector<int>parent(v,-1);
+        
+    queue<int>q;
+    q.push(src);
+    visit[src]=true;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto x:adj[node]){
+            if(visit[x]==false){
+                visit[x]=true;
+                parent[x]=node;
+                q.push(x);
+            }else if(parent[x]!=node){ //cycle detected
+                return true;
+            }
+        }
+    }   
+    return false;
 }
