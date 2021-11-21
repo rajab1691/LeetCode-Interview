ec739e2140acab4189b73a8f4599d36565676bda
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-11-21 13:27:13 +0530

    Update arrival.cpp

--- Graph/arrival.cpp
+++ Graph/arrival.cpp
@@ -1,1 +1,49 @@
+// https://www.techiedelight.com/arrival-departure-time-vertices-dfs/
+
+
+void dfs(int src,vi adj[],vi &arrival,vi &dep,int &cnt,vi &visit){
+
+    visit[src]=1;
+    arrival[src]=++cnt;
+
+    for(auto x:adj[src]){
+        if(visit[x]==0){
+            dfs(x,adj,arrival,dep,cnt,visit);
+        }
+    }
+
+    dep[src]=++cnt;
+}
+
+int32_t main() {
+
+    int n,m;
+    cin>>n>>m;
+
+    vector<int>adj[n];
+
+    while(m--){
+        int a,b;
+        cin>>a>>b;
+        adj[a].pb(b);
+    }
+
+    vector<int>arrival(n);
+    vector<int>dep(n);
+    vector<int>visit(n,0);
+    int cnt=-1;
+    
+    fo(i,n){
+        if(visit[i]==0){
+            dfs(i,adj,arrival,dep,cnt,visit);
+        }
+    }
+
+    fo(i,n){
+        cout<<i<<" "<<arrival[i]<<" "<<dep[i]<<endl;
+    }
+}
+
+
+
 
