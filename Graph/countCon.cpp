c9f2c9877bb768ed7689381e34d74cb71ddae178
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-11-21 11:23:04 +0530

    Update countCon.cpp

--- Graph/countCon.cpp
+++ Graph/countCon.cpp
@@ -4,11 +4,26 @@
     
 */
 
- void dfs(int src, vector<bool>&visit) {
+ void dfs(int src, vector<bool>&visit,vector<int>adj[]) {
         visit[src] = true;
         for (auto x : adj[src]) {
             if (visit[x] == false) {
                 dfs(x, visit);
             }
         }
+}
+
+int countComp(vector<int>adj[],int v){
+
+    int count=0;
+    vector<bool>visit(v,false);
+
+    for(int i=0;i<v;i++){   
+
+        if(visit[i]==false){
+            dfs(i,visit,adj);
+            count++;
+        }
+    }
+    return count;
 }
