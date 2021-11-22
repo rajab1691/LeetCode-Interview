4cec9386b97da2b668a7dff95625cfd92d88bb63
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-11-22 20:30:29 +0530

    Update leftview.cpp

--- BinaryTree/leftview.cpp
+++ BinaryTree/leftview.cpp
@@ -1,1 +1,40 @@
+// 
 
+/*
+      Use LOT
+      T.C-O(n)
+*/
+
+void leftView(Node* root){
+
+    if(root==NULL){
+        return;
+    }
+
+    queue<Node*>q;
+    q.push(root);
+   
+    while(!q.empty()){
+
+        int size=q.size();
+        int count=size;
+        while(size--){
+
+            Node* curr=q.front();
+            q.pop();
+
+            if(count-1==size){
+                cout<<curr->data<<" ";
+            }
+
+            if(curr->left){
+                q.push(curr->left);
+            }
+
+            if(curr->right){
+                q.push(curr->right);
+            }
+
+        }
+    }
+}
