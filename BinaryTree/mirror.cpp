5b88af796f6c08639d662d3e3e51f0325f8c5346
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-11-22 14:04:37 +0530

    Update mirror.cpp

--- BinaryTree/mirror.cpp
+++ BinaryTree/mirror.cpp
@@ -26,3 +26,46 @@ public:
     
     }
 };
+
+/*
+    Using Iteration
+    ->just use two pointer left and right
+*/
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        TreeNode *left,*right;
+        if(root==NULL){
+            return true;
+        }
+        
+        queue<TreeNode*>q;
+        q.push(root->left);
+        q.push(root->right);
+        
+        while(!q.empty()){
+            
+            left=q.front();
+            q.pop();
+            
+            right=q.front();
+            q.pop();
+            
+            if(left==NULL and right==NULL)
+                continue;
+            
+            if(left==NULL or right==NULL)
+                return false;
+            
+            if(left->val!=right->val)
+                return false;
+            
+            q.push(left->left);
+            q.push(right->right);
+            q.push(left->right);
+            q.push(right->left);
+        }
+        return true;
+    }
+};
