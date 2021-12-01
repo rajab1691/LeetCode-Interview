748892255212100b048869c9a78efa9eddcc89ef
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-12-01 12:12:20 +0530

    Update graphatree.cpp

--- Graph/graphatree.cpp
+++ Graph/graphatree.cpp
@@ -1,1 +1,23 @@
+/*
+  Graph is a tree:
+    ->No cycle
+    ->Undirected
+    ->Connected Graph
+*/
 
+bool isTree(vector<int>adj[],int n){
+
+    vector<bool>visit(n,false);
+
+    int parent=-1;
+    //cycle detection
+    if(isCycle(0,adj,parent,visit))
+        return false;
+
+    //check for connected components
+    for(int i=0;i<n;i++){
+        if(visit[i]==false)
+            return false;
+    }
+    return true;
+}
