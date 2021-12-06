0d0c59492102b59e21abfa4b90f03ffbf7ab7462
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-12-06 11:23:22 +0530

    Update ssspbfs.cpp

--- Graph/ssspbfs.cpp
+++ Graph/ssspbfs.cpp
@@ -1,1 +1,36 @@
+/*
+  ->Undirected/Directed Connected Graph
+  ->Unweighted
+  ->Just do normal bfs maintaining weight:dist[NewNode]=dist[node]+1;
+  T.C-O(v+e)
+  S.C-O(v)
+*/
 
+//dist[]={0,0,...}-initially,visited[]={f,f,...}
+//assume src=0;
+
+void ssspBFS(vector<int>adj[],int dist[],bool visited[],int n){
+
+    //dist of source is zero
+    dist[src]=0;
+
+    queue<int>q;
+
+    q.push(src);
+    visited[src]=true;
+
+    while(!pq.empty()){
+
+        int node=pq.front();
+        pq.pop();
+
+        for(auto x:adj[node]){
+            if(visited[x]==false){
+                visited[x]=true;
+                //add 1 everytime it visit a node levelwise
+                dist[x]=dist[node]+1;
+                q.push(x);
+            }
+        }
+    }
+}
