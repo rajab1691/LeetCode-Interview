5a5d576d174ac4e3719e98f938aff76fb99d71cb
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-12-08 19:32:00 +0530

    Update RatInaMaze.cpp

--- Backtracking/RatInaMaze.cpp
+++ Backtracking/RatInaMaze.cpp
@@ -1,46 +1,35 @@
 // https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1#
-/*
-  
-      
-  Time Complexity: O(3^(n^2))=As there are N^2 cells from each cell there are 3 unvisited neighbouring cells. So the time complexity O(3^(N^2).
-  Auxiliary Space: O(3^(n^2))=As there can be atmost 3^(n^2) cells in the answer so the space complexity is O(3^(n^2)).
-  
-*/
-vector<string>res;
-vector<vector<int>>vis(5, vector<int> (5,0));
+
+//Time Complexity: O(3^(n^2))=As there are N^2 cells from each cell there are 3 unvisited neighbouring cells. So the time complexity O(3^(N^2).
+
 class Solution{
     public:
-    void path(vector<vector<int>>&m,int i,int j,string r,int n){
+    void path(vector<vector<int>>&m,int i,int j,string r,int n,vector<string>&res){
         if(i==n-1 and j==n-1){
             res.push_back(r);
             return;
         }
-        if(m[i][j]==0 or vis[i][j]==1)return;
+        if(m[i][j]==0)return;
         
         //choose
-        vis[i][j]=1;
+        m[i][j]=0;
       
         //exploring
-        if(i>0)path(m,i-1,j,r+'U',n);
-        if(i<n-1)path(m,i+1,j,r+'D',n);
-        if(j>0)path(m,i,j-1,r+'L',n);
-        if(j<n-1)path(m,i,j+1,r+'R',n);
+        if(i>0)path(m,i-1,j,r+'U',n,res);
+        if(i<n-1)path(m,i+1,j,r+'D',n,res);
+        if(j>0)path(m,i,j-1,r+'L',n,res);
+        if(j<n-1)path(m,i,j+1,r+'R',n,res);
       
         //backtrack or unchoose
-        vis[i][j]=0;
+        m[i][j]=1;
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                vis[i][j]=0;
-            }
-        }
-        res.clear();
+       
+        vector<string>res;
         //if source or dest cell is not visitable then just return 
         if(m[0][0]==0 or m[n-1][n-1]==0)return res;
         
-        path(m,0,0,"",n);
+        path(m,0,0,"",n,res);
         sort(res.begin(),res.end());
         return res;
     }
