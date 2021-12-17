9d59733a0e52b4aec70f73e6e8f4f899e7800951
Raaj <48174962+rajab1691@users.noreply.github.com>
2021-12-17 17:29:52 +0530

    Create Postorder.cpp

---
+++ N-ary Tree/Postorder.cpp
@@ -0,0 +1,17 @@
+void utilPost(Node * root, vector < int > & res) {
+
+  if (root == NULL) return;
+
+  for (int i = 0; i < root -> children.size(); i++) {
+    utilPost(root -> children[i], res);
+  }
+  res.push_back(root -> val);
+}
+vector < int > postorder(Node * root) {
+  vector < int > res;
+  if (root == NULL)
+    return res;
+
+  utilPost(root, res);
+  return res;
+}
